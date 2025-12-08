4f4cdb7c8e188d5882f69574033ac532fa979e85
Ogechukwu Ezenwa <107501022+Gechyb@users.noreply.github.com>
2025-12-08 16:46:01 +0100

    Add training scripts for CRF with glove embeddings

--- scripts/train_crf.sh
+++ scripts/train_crf.sh
@@ -2,3 +2,6 @@
 python -m src.train_crf --domain restaurants > results/crf/restaurants.txt
 python -m src.train_crf --domain laptops > results/crf/laptops.txt
 
+# with glove 
+python -m src.train_crf --domain restaurants --use_glove > results/crf/restaurants_glove.txt
+python -m src.train_crf --domain laptops --use_glove > results/crf/laptops_glove.txt
